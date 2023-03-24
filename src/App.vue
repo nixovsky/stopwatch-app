8aa03cbdd9d7c619b551499b43a620eca171c38d
Nikita <92846803+nixovsky@users.noreply.github.com>
2023-03-24 19:28:17 +0300

    stopwatch layout

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,64 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div class="stopwatch__container">
+  <stopwatch v-for="stopwatch in stopwatchCount" :key="stopwatch"></stopwatch>
+  <button @click="stopwatchCount += 1;" class="add-wtopwatch__btn">+</button>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import stopwatch from './components/stopwatch.vue'
 
 export default {
-  name: 'App',
+  data() {
+    return {
+      stopwatchCount: 1,
+    }
+  },
+  methods: {
+
+  },
   components: {
-    HelloWorld
+    stopwatch
   }
 }
 </script>
 
 <style>
+@import url('https://fonts.cdnfonts.com/css/gotham-pro');
+
+body{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  background: #353638;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 0 10%;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: 'Gotham Pro', sans-serif;
+  color: white;
+}
+.stopwatch__container{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+}
+.add-wtopwatch__btn{
+  background: #696969;
+  border: 0;
+  width: 225px;
+  height: 120px;
+  position: relative;
+  font-size: 60px;
+  color: #9E9E9E;
+  transition: 0.1s;
+  margin: 15px 15px;
+}
+.add-wtopwatch__btn:hover{
+  background: #747474;
+  color: #bdbdbd;
 }
 </style>
---
+++ src/components/stopwatch.vue
@@ -0,0 +1,119 @@
+<template>
+    <div class="stopwatches">
+        <div class="stopwatch">
+            <span class="stopwatch__time">{{time}}</span>
+        </div>
+        <div class="stopwatch__control">
+            <button v-if="isPlay" class="pause"></button>
+            <button v-else class="play"></button>
+            <button class="stop"></button>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return{
+            time: "10:10:100",
+            isPlay: false,
+        }   
+    },
+}
+</script>
+
+<style>
+    .stopwatches{
+        width: 225px;
+        height: 120px;
+        background: #696969;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        position: relative;
+        margin: 15px 15px;
+        position: relative;
+    }
+    .stopwatches::after{
+        content: '';
+        position: absolute;
+        width: 100%;
+        height: 2px;
+        top: 50%;
+        left: 0;
+        background: white;
+    }
+    .stopwatch{
+        margin-bottom: 40px;
+    }
+    .stopwatch__time{
+        font-weight: 400;
+        font-size: 20px;
+    }
+    .stopwatch__control button{
+        background: 0;
+        border: 0;
+        color: white;
+        position: relative;
+        width: 20px;
+        height: 20px;
+        margin-right: 20px;
+    }
+    .stopwatch__control button:last-child{
+        margin-right: 0;
+    }
+    .play::before{
+        content: '';
+        position: absolute;
+        width: 0;
+        height: 0;
+        border-top: 10px solid transparent;
+        border-left: 20px solid white;
+        border-bottom: 10px solid transparent;
+        left: 0;
+        top: 0;
+        cursor: pointer;
+    }
+    .pause::after{
+        content: '';
+        position: absolute;
+        width: 7px;
+        height: 100%;
+        top: 0;
+        right: 0;
+        background: white;
+        cursor: pointer;
+    }
+    .pause::before{
+        content: '';
+        position: absolute;
+        width: 7px;
+        height: 100%;
+        top: 0;
+        left: 0;
+        background: white;
+        cursor: pointer;
+    }
+    .stop::before{
+        content: '';
+        position: absolute;
+        width: 20px;
+        height: 20px;
+        top: 0;
+        left: 0;
+        background: white;
+        cursor: pointer;
+    }
+    .inactive::before{
+        content: '';
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        top: 0;
+        left: 0;
+        z-index: 100;
+        background: rgba(0, 0, 0, 0.2);
+    }
+</style>
